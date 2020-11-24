5f2ad261a880edd5eee4a33e6024dfcbb5fa834f
Benedict Romp <benedict.romp@googlemail.com>
2020-11-24 17:13:34 +0100

    Anpassung

--- src/Application.js
+++ src/Application.js
@@ -2,6 +2,7 @@ const express = require("express");
 const fileupload = require("express-fileupload");
 const Connections = require("./Database/Connections");
 const Config = require("./Config");
+const Logger = require("./Logger");
 var app = express();
 
 exports.boot = () => {
@@ -31,11 +32,20 @@ exports.boot = () => {
     // Load routes
     require("../../../routes");
 
+    app.use((err, req, res, next) => {
+        console.log("ERROR:");
+        console.log(err);
+        res.status(500).json({ status: "error", error: error });
+    });
+    console.log("LOG-INIT");
+
     // Start express-app
     const port = Config.get("app.port");
     app.listen(port, () => {
         console.log(`Example app listening at http://localhost:${port}`);
     });
+
+    return app;
 };
 
 exports.app = () => {
