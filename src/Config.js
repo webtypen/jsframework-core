d686673566cf7951ab62d12c0de385e769f8ffa1
TorrnexT <benedict.romp@googlemail.com>
2022-03-01 09:44:02 +0100

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,16 +1,22 @@
 "use strict";
 const { get, set } = require("lodash");
-const Logger = require("./Logger");
 
 var configDir = "../../../../config/";
 var files = ["app", "auth", "database"];
 var config = {};
 
-exports.load = () => {
-    for (let i in files) {
-        const conf = require(configDir + files[i]);
-        if (conf) {
-            config[files[i]] = conf;
+exports.load = (path) => {
+    if (path) {
+        const tempConfig = require(configDir + path);
+        if (tempConfig) {
+            config[path] = tempConfig;
+        }
+    } else {
+        for (let i in files) {
+            const conf = require(configDir + files[i]);
+            if (conf) {
+                config[files[i]] = conf;
+            }
         }
     }
 };
